cd56b6221aca06a7d5ed03c34d60d4058f9ffc59
AkitaNin <littlerainsz@gmail.com>
2024-10-14 17:31:41 +0700

    add modified

--- src/test/java/nintester/com/runtest.java
+++ src/test/java/nintester/com/runtest.java
@@ -1,4 +1,19 @@
 package nintester.com;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class runtest {
+    public static void main(String[] args) {
+        System.out.println("Hello this is new style");
+
+        WebDriver driverCr = new ChromeDriver();
+        driverCr.get("https://www.youtube.com/");
+
+       /* WebDriver driverFx = new FirefoxDriver();
+        driverFx.get("https://www.youtube.com/");*/
+
+
+    }
 }
--- src/test/java/nintester/com/testng_test.java
+++ src/test/java/nintester/com/testng_test.java
@@ -1,4 +1,30 @@
 package nintester.com;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class testng_test {
+
+    WebDriver driver;
+
+    @Test
+    public void test01(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+        driver.get("https://www.youtube.com/");
+        driver.close();
+    }
+    @Test
+    public void test02(){
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
+        driver.get("https://www.youtube.com/");
+        driver.quit();
+    }
 }
